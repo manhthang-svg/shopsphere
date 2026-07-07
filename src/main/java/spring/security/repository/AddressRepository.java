976321ab39be5b3bac86868c724a5222e999d2c4
Phung Manh Thang <phungmanhthang0411@gmail.com>
2026-07-07 10:10:52 +0700

    feat: handle logic default address when adding an address

--- src/main/java/spring/security/repository/AddressRepository.java
+++ src/main/java/spring/security/repository/AddressRepository.java
@@ -1,12 +1,25 @@
 package spring.security.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import spring.security.entity.Address;
+import spring.security.entity.Users;
 
 import java.util.List;
 
 @Repository
 public interface AddressRepository extends JpaRepository<Address,Long> {
     List<Address> findByUserId(Long id);
+
+    Long countAddressByUser(Users user);
+    @Modifying
+    @Query("""
+       UPDATE Address a
+       SET a.isDefault = false
+       WHERE a.user = :user 
+""")
+    int clearDefaultAddress(@Param("user") Users user);
 }
